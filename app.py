22d00fc32e1f9f8658f6c5b14b5def8cbc3623b3
Cristian Suarez Vera <cristian.suarez.ulgpc@gmail.com>
2021-06-29 17:53:02 +0100

    extract to function format final message

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ app._static_folder = '/static/'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    next_page = ''  # to hold next_page token, empty for first page
+    next_page = ''
     videos_counter = 0
     total_playlist_length = timedelta(0)
     display_text = []
@@ -57,12 +57,10 @@ def home():
         vid_list = []
 
         try:
-            # make first request to get list of all video_id one page of response
             results = json.loads(requests.get(URL1.format(API_KEY, PLAYLIST_ID) + next_page).text)
 
-            # add all ids to vid_list
-            for x in results['items']:
-                vid_list.append(x['contentDetails']['videoId'])
+            for video in results['items']:
+                vid_list.append(video['contentDetails']['videoId'])
 
         except KeyError:
             display_text = [results['error']['message']]
@@ -71,26 +69,20 @@ def home():
         # now vid_list contains list of all videos in playlist one page of response
         url_list = ','.join(vid_list)
         # updating counter
-        videos_counter += len(vid_list)
 
         try:
             # now to get the durations of all videos in url_list
             op = json.loads(requests.get(URL2.format(url_list, API_KEY)).text)
 
             # add all the durations to total_playlist_length
-            for x in op['items']:
-                total_playlist_length += isodate.parse_duration(x['contentDetails']['duration'])
-
-            total_weeks_of_the_year = datetime.date(2021, 12, 31).isocalendar()[1]
-            current_week_number = datetime.datetime.today().isocalendar()[1]
-            weeks_to_end_the_year = total_weeks_of_the_year - current_week_number
-            total_playlist_hours = total_playlist_length.days * 24 + total_playlist_length.seconds / 3600
-            missing_challenge_hours = (100 - total_playlist_hours)
+            for video in op['items']:
+                total_playlist_length += isodate.parse_duration(video['contentDetails']['duration'])
 
         except KeyError:
-            display_text = [results['error']['message']]
+            display_text = [op['error']['message']]
             break
 
+        videos_counter += len(vid_list)
         # if 'nextPageToken' is not in results, it means it is the last page of the response
         # otherwise, or if the videos_counter has not yet exceeded 500
         if 'nextPageToken' in results and videos_counter < 500:
@@ -98,16 +90,26 @@ def home():
         else:
             if videos_counter >= 500:
                 display_text = ['No of videos limited to 500.']
-            display_text += [
-                'Número de vídeos: ' + str(videos_counter),
-                'Tamaño total de la lista: ' + parse_time(total_playlist_length),
-                'Esto significa que tenemos un {:.2f}% del reto conseguido'.format(total_playlist_hours),
-                'Y que necesitamos hacer al menos {:.2f} horas a la semana para llegar a tiempo'.format(
-                    missing_challenge_hours / weeks_to_end_the_year),
-            ]
+            display_text += format_message(total_playlist_length, videos_counter)
             break
 
     return render_template("home.html", display_text=display_text)
+
+
+def format_message(total_playlist_length, videos_counter):
+    total_weeks_of_the_year = datetime.date(2021, 12, 31).isocalendar()[1]
+    current_week_number = datetime.datetime.today().isocalendar()[1]
+    weeks_to_end_the_year = total_weeks_of_the_year - current_week_number
+    total_playlist_hours = total_playlist_length.days * 24 + total_playlist_length.seconds / 3600
+    missing_challenge_hours = (100 - total_playlist_hours)
+
+    return [
+        'Número de vídeos: ' + str(videos_counter),
+        'Tamaño total de la lista: ' + parse_time(total_playlist_length),
+        'Esto significa que tenemos un {:.2f}% del reto conseguido'.format(total_playlist_hours),
+        'Y que necesitamos hacer al menos {:.2f} horas a la semana para llegar a tiempo'.format(
+            missing_challenge_hours / weeks_to_end_the_year),
+    ]
 
 
 if __name__ == "__main__":
